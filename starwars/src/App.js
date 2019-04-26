a666276c3c1ee537d0b1094c4481409aca2f7f76
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-04-26 12:46:54 -0400

    Updated App.js to display character info

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,12 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const StarWarsChars = props => {
+  return (
+    <div className="char-card">
+
+      <div className="char-info">
+        <p>
+          <h2>{props.charsProp.name} </h2> 
+        </p>
+        <p>
+          <strong>Birth Year:</strong> {props.charsProp.birth_year}
+        </p>
+        <p>
+          <strong>Gender:</strong> {props.charsProp.gender}
+        </p>
+        <p>
+          <strong>Height:</strong> {props.charsProp.height}
+        </p>
+        <p>
+          <strong>Mass:</strong> {props.charsProp.mass}
+        </p>
+        <p>
+          <strong>Eye Color:</strong> {props.charsProp.eye_color}
+        </p>
+        <p>
+          <strong>Hair Color:</strong> {props.charsProp.hair_color}
+        </p>
+        <p>
+          <strong>Skin Color:</strong> {props.charsProp.skin_color}
+        </p>
+      </div>
+
+    </div>
+  );
+};
+
+
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      character: {
+        name: "",
+        birth_year: "",
+        gender: "",
+        height: "",
+        mass: "",
+        eye_color: "",
+        hair_color: "",
+        skin_color: "",
+      }
     };
+
   }
 
   componentDidMount() {
@@ -29,13 +77,28 @@ class App extends Component {
       });
   };
 
+
   render() {
     return (
       <div className="App">
+
         <h1 className="Header">React Wars</h1>
+
+
+        <div className="char-list">
+
+          {this.state.starwarsChars.map(charsFromMap => {
+            return <StarWarsChars charsProp={charsFromMap} />
+          })}
+
+        </div>
+
+
       </div>
+
     );
   }
 }
 
 export default App;
+
